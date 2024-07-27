5a80af949bbe5555d569833879e6f1a0f2f27aee
Lareta064 <lareta064@gmail.com>
2024-07-27 19:27:10 +0300

    report-slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (){
+	$('.lazy').lazy();
+
+	var reportSlider = new Swiper('.report-swiper', {
+		spaceBetween:20,
+		speed:1000,
+		
+		 navigation: {
+			nextEl: ".report-next",
+			prevEl: ".report-prev",
+		},
+		breakpoints: {
+			768: {
+			
+			spaceBetween: 70,
+			},
+			1200: {
+			
+			spaceBetween: 140,
+			},
+		}
+	});
 });
